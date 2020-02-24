e3f46e84ed987fc2c6a94f4f099ec272e23473bb
BrooklynWelsh <brooklynwelsh95@gmail.com>
2020-02-24 17:13:55 -0500

    First commit to GitHub

---
+++ src/welsh_bd/p2/AnagramGenerator.java
@@ -0,0 +1,5 @@
+package welsh_bd.p2;
+
+public class AnagramGenerator {
+
+}
---
+++ src/welsh_bd/p2/MS1Main.java
@@ -0,0 +1,10 @@
+package welsh_bd.p2;
+
+public class MS1Main {
+	
+	public static void main(String[] args) {
+		AlphaCount input = new AlphaCount(args[0]);
+		
+		
+	}
+}
