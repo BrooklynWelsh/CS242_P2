c593b0c743e9b5ddaf6dba52707734c8e2a6839b
Wahl_D <Wahl_D@L-HOBB-118-0001.lynchburg.edu>
2020-02-25 13:30:15 -0500

    anagrams package

---
+++ src/anagrams/AlphaCount.java
@@ -0,0 +1,19 @@
+package anagrams;
+
+
+public class AlphaCount {
+	public AlphaCount(){
+		
+	}
+	
+	public AlphaCount(String input) {
+		inputString = input.replaceAll("[0-9]", "");	// This program only keeps characters, not numbers 
+	}
+	
+	public AlphaCount add(AlphaCount other) {
+		for()
+	}
+	
+	ULHashMap<Character, Integer> charactersToCounts;
+	String inputString;
+}
