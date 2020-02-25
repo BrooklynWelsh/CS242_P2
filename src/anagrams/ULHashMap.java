ad12cabae6d02e7c1f8ccdc42bb7976f44ac3647
welsh_bd <welsh_bd@L-HOBB-118-002.lynchburg.edu>
2020-02-25 14:46:59 -0500

    All functions except clone() and iterator() completed.

--- src/anagrams/ULHashMap.java
+++ src/anagrams/ULHashMap.java
@@ -3,7 +3,7 @@ package anagrams;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class ULHashMap<K,V> implements Cloneable, Iterable{
+public class ULHashMap<K,V> implements Cloneable, Iterable<ULHashMap.Mapping<K, V>>{
 	
 	public ULHashMap() {
 		// Table size must be prime
@@ -27,7 +27,7 @@ public class ULHashMap<K,V> implements Cloneable, Iterable{
 	}
 	 
 	public void clear() {
-		for(LinkedList<V> list : buckets) {
+		for(LinkedList<Mapping<K, V>> list : buckets) {
 			list.clear();
 		}
 	}
@@ -52,52 +52,156 @@ public class ULHashMap<K,V> implements Cloneable, Iterable{
 	}
 	
 	public boolean equals(Object otherObject) {
-		
+		return this.equals(otherObject);
 	}
 	
 	public void erase(K key) {
+		int bucketIndex = Math.abs(key.hashCode()) % tableSize;
 		
+		for(Mapping<K,V> mapping : buckets[bucketIndex]) {
+			if(mapping.key.equals(key)) {
+				buckets[bucketIndex].remove(mapping);
+				size--;
+			}
+		}
 	}
 	
 	public void insert(K key, V value) {
+		int bucketIndex = Math.abs(key.hashCode()) % tableSize;
 		
+		// If bucket is empty just add new mapping to the first space in bucket
+		if(buckets[bucketIndex].size() == 0) {	
+			buckets[bucketIndex].addFirst(new Mapping<K,V>(key, value));
+			size++;
+		}
+		
+		//Else, iterate through bucket to see if K is already there
+		else {
+			for(Mapping<K,V> mapping : buckets[bucketIndex]) {
+				if(mapping.key.equals(key))	throw new DuplicateKeyException();
+			}
+			// If we didn't throw, insert key at end of bucket
+			buckets[bucketIndex].addLast(new Mapping<K,V>(key, value));
+			size++;
+			
+			// Now check that we haven't filled our list. If so, find next prime number
+			if(size == tableSize) {
+				int newTableSize = tableSize + 1;
+				while(!isPrime(newTableSize)) ++newTableSize;
+				tableSize = newTableSize;
+			}
+		}
 	}
 	
 	public boolean isPrime(int possibleSize) {
+		boolean retVal = true;
+		int possibleFactor = 1;
 		
+		// While we have more numbers to check AND we haven't already found that possibleSize is not prime...
+		while(possibleFactor < possibleSize && retVal == true) {
+			// Check if possibleFactor is actually a factor and if not increment by one
+			if(possibleSize % possibleFactor == 0)	retVal = false;
+			possibleFactor++;
+		}
+		
+		return retVal;
 	}
 	
+	@Override
 	public Iterator<ULHashMap.Mapping<K,V>> iterator() {
-		
+		return new Iterator<ULHashMap.Mapping<K,V>>(){
+
+			@Override
+			public boolean hasNext() {
+				
+			}
+
+			@Override
+			public Mapping<K, V> next() {
+				if(!hasNext()) throw new java.util.NoSuchElementException();
+				
+				Mapping<K,V> iter = buckets[0].getFirst(); // Start at very first element of first bucket
+				
+			}
+			
+			@Override
+			public void remove() {
+				throw new java.lang.UnsupportedOperationException();
+			}
+		};
 	}
 	
 	public V lookup(K key) {
+		int bucketIndex = Math.abs(key.hashCode()) % tableSize;
+		V retVal = null;
+		// If there's only one item it has to be it
+		if(buckets[bucketIndex].size() == 1) retVal = buckets[bucketIndex].getFirst().getValue();
+		else {
+			for(Mapping<K,V> mapping : buckets[bucketIndex]) {
+				if(mapping.getKey().equals(key))	retVal = mapping.getValue();
+			}
+		}
 		
+		return retVal;
 	}
 	
 	public void put(K key, V value) {
+		int bucketIndex = Math.abs(key.hashCode()) % tableSize;
 		
+		// If bucket is empty just add new mapping to the first space in bucket
+		if(buckets[bucketIndex].size() == 0) {	
+			buckets[bucketIndex].addFirst(new Mapping<K,V>(key, value));
+			size++;
+		}
+		
+		//Else, iterate through bucket to see if K is already there. If so, overwrite
+		else {
+			boolean wasInserted = false;
+			for(Mapping<K,V> mapping : buckets[bucketIndex]) {
+				if(mapping.key.equals(key)) {
+					mapping.key = key;
+					mapping.value = value;
+					wasInserted = true;
+				}
+				
+				// If not insert key at end of bucket
+				if(!wasInserted) {
+					buckets[bucketIndex].addLast(new Mapping<K,V>(key, value));
+					size++;
+				}
+			}
+			
+			// Now check that we haven't filled our list. If so, find next prime number
+			if(size == tableSize) {
+				int newTableSize = tableSize + 1;
+				while(!isPrime(newTableSize)) ++newTableSize;
+				tableSize = newTableSize;
+			}
+		}
 	}
 	
 	public int size() {
-		
+		return size;
 	}
 	
 	public int tableSize() {
-		
+		return tableSize;
 	}
 
-	@Override
-	public Iterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 	 
-	int size;
-	int tableSize;
-	LinkedList<Mapping<K,V>> buckets[]; 
+	private int size;
+	private int tableSize;
+	private LinkedList<Mapping<K,V>> buckets[]; 
 	
 	public static class Mapping<K,V>{
+		
+		public Mapping(){}
+		
+		public Mapping(K Key, V Value) {
+			this.key = Key;
+			this.value = Value;
+		}
 		
 		public K getKey() {
 			return key;
@@ -107,8 +211,8 @@ public class ULHashMap<K,V> implements Cloneable, Iterable{
 			return value;
 		}
 		
-		private K key;
-		private V value;
+		public K key;
+		public V value;
 	}
 	
 }
