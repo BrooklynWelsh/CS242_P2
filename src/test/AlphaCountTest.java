257cc7430b23c8bc162b4b07c05ab23c22ef7005
WELSH_BD <WELSH_BD@L-HOBB-118-002.lynchburg.edu>
2020-02-27 13:30:39 -0500

    Added test file.

---
+++ src/test/AlphaCountTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import anagrams.AlphaCount;
+
+class AlphaCountTest {
+
+	@Test
+	void testIsSubset() {
+		AlphaCount alpha = new AlphaCount("Test");
+		fail("Not yet implemented");
+	}
+
+}
