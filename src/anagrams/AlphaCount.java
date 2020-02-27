0f7307f5bb8cd38e0790c382ab79131571db4c81
WELSH_BD <WELSH_BD@L-HOBB-118-002.lynchburg.edu>
2020-02-27 13:24:51 -0500

    Change to isSubset()

--- src/anagrams/AlphaCount.java
+++ src/anagrams/AlphaCount.java
@@ -73,7 +73,7 @@ public class AlphaCount {
 	public boolean isSubset(AlphaCount other) {
 		boolean isSubset = true;
 		for(Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
-			if(other.getLetter(entry.getKey()) > entry.getValue()) {
+			if(entry.getValue() > this.getLetter(entry.getKey())) {
 				isSubset = false;
 			}
 		}
--- src/anagrams/MS2Main.java
+++ src/anagrams/MS2Main.java
@@ -1,10 +1,32 @@
 package anagrams;
 
+import java.util.Scanner;
+
 public class MS2Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String testString = null;
+		
+		System.out.print("Please provide a new test string (0 to exit): ");
+		testString = scanner.nextLine();
+		
+		while (!testString.equals("0")) {
 
+			// Convert it into an alphaCount
+			AlphaCount alphaCount = new AlphaCount(testString);
+
+			// Print out the counts
+			System.out.println("Now printing the count : letter mappings for alphaCount...");
+			System.out.println(alphaCount.toString());
+
+			// Create and print the hashCode for alphaCount
+			int theHash = alphaCount.hashCode();
+			System.out.println("alphaCount hashCode : " + theHash);
+
+			System.out.print("Please provide a new test string (0 to exit): ");
+			testString = scanner.nextLine();
+		}
 	}
 
 }
